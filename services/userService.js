82cb37d1fdbb87a86c8ea2603769a7316bcdc251
UtsavGohel <utsavgohel2002@gmail.com>
2025-05-28 13:06:50 +0530

    feat(add-user): implement add new user endpoint

--- services/userService.js
+++ services/userService.js
@@ -1,6 +1,8 @@
 // Import AWS SDK , UUID for unique ID generation
 const AWS = require("aws-sdk");
 
+const { v4: uuidv4 } = require("uuid");
+
 // Create a DynamoDB DocumentClient instance
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
@@ -17,5 +19,28 @@ exports.getAllUsers = async (req, res) => {
     res.status(200).json(result.Items); // returns all user records
   } catch (err) {
     res.status(500).json({ error: "Error fetching users", details: err });
+  }
+};
+
+/**
+ * Create a new user
+ * @route POST /users
+ */
+exports.createUser = async (req, res) => {
+  console.log("req.body =", req.body);
+
+  const { name, email, age, phone, address } = req.body;
+  const userId = uuidv4();
+
+  const params = {
+    TableName: TABLE_NAME,
+    Item: { userId, name, email, age, phone, address },
+  };
+
+  try {
+    await dynamoDb.put(params).promise();
+    res.status(201).json({ userId, name, email, age, phone, address });
+  } catch (err) {
+    res.status(500).json({ error: "Could not create user", details: err });
   }
 };
